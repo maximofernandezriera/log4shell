15f6541acc9bc3cf443368531978c62fa1182159
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2022-01-09 22:23:41 +0100

    Modification for PePS

--- src/main/java/HelloLog.java
+++ src/main/java/HelloLog.java
@@ -6,13 +6,10 @@ public class HelloLog {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        String userInput = "${jndi:http://localhost/AAAA/BBBB}";
 
-        // passing user input into the logger
-        logger.info("Test: "+userInput);
+        // passing first argument
+        logger.error(args[0]);
 
-        // %m{nolookups} has no effect for the following line
-        // logger.printf(Level.INFO,"Test: %s", userInput);
     }
 }
 
